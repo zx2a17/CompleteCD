6aa5d74c746d74d540653692d67e5bc4ef1c599e
zx2a17 <48633832+zx2a17@users.noreply.github.com>
2024-06-28 10:31:38 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,18 +2,18 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
-    }
-  }
-  required_version = ">= 1.1.0"
-}
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "4.52.0"
+#    }
+#    random = {
+#      source  = "hashicorp/random"
+#      version = "3.4.3"
+#    }
+#  }
+#  required_version = ">= 1.1.0"
+#}
 
 provider "aws" {
   region = "eu-central-1"
