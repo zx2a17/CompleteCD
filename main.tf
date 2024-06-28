0edf38f18a0b69d299ead5da71c6ae33973ab88b
zx2a17 <48633832+zx2a17@users.noreply.github.com>
2024-06-28 10:59:48 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-#terraform {
-#  required_providers {
-#    aws = {
-#      source  = "hashicorp/aws"
-#      version = "4.52.0"
-#    }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
 #    random = {
 #      source  = "hashicorp/random"
 #      version = "3.4.3"
 #    }
 #  }
 #  required_version = ">= 1.1.0"
-#}
+}
 
 provider "aws" {
   region = "eu-central-1"
