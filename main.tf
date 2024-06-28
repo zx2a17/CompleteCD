d0d58f36e464783166d91bb701927fce415a45f1
zx2a17 <48633832+zx2a17@users.noreply.github.com>
2024-06-28 11:38:33 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,9 +47,14 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = [aws_security_group.web-sg.id]
   user_data = <<-EOF
               #!/bin/bash
-              sudo yum update
+              sudo yum update -y
               sudo yum upgrade
               sudo yum install -y git htop wget
+              curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+              export NVM_DIR="$HOME/.nvm"
+              [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+              [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+              nvm install --lts # Latest stable node js server version
               EOF
 }
               # #!/bin/bash
