c3d48ae25a9aac761e938356860eb377396c7521
zx2a17 <48633832+zx2a17@users.noreply.github.com>
2024-07-15 09:03:13 +0800

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,7 +2,7 @@
 echo 'run after_install.sh: ' >> /home/ec2-user/complete-cd/deploy.log
 
 echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/complete-cd/deploy.log
-cd /home/ec2-user/nodejs-aws-codedeploy-pipeline >> /home/ec2-user/complete-cd/deploy.log
+cd /home/ec2-user/complete-cd >> /home/ec2-user/complete-cd/deploy.log
 
 echo 'npm install' >> /home/ec2-user/complete-cd/deploy.log 
 npm install >> /home/ec2-user/complete-cd/deploy.log
